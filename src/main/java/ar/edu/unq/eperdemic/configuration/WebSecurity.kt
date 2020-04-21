d3f5c86cf587502c949276243e57afdc9de0d14b
Ivan Dominikow <Dominikowivan@gmail.com>
2020-04-21 18:45:09 -0300

    added WebSecurity to work around cors

---
+++ src/main/java/ar/edu/unq/eperdemic/configuration/WebSecurity.kt
@@ -0,0 +1,16 @@
+package ar.edu.unq.eperdemic.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+
+@Configuration
+@EnableWebSecurity
+class WebSecurity : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http.cors()
+    }
+}
