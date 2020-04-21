5aad142f448c249ff09904f06f35cfa0b5e5b013
Ivan Dominikow <Dominikowivan@gmail.com>
2020-04-21 18:59:08 -0300

    trying luck with other cors configuration

--- src/main/java/ar/edu/unq/eperdemic/configuration/WebSecurity.kt
+++ src/main/java/ar/edu/unq/eperdemic/configuration/WebSecurity.kt
@@ -1,9 +1,14 @@
 package ar.edu.unq.eperdemic.configuration
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import java.util.*
 
 
 @Configuration
@@ -12,5 +17,17 @@ class WebSecurity : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
         http.cors()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = Arrays.asList("*")
+        configuration.allowedMethods = Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+        configuration.allowedHeaders = Arrays.asList("authorization", "content-type", "x-auth-token")
+        configuration.exposedHeaders = Arrays.asList("x-auth-token")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
